3bf44c5eb8fe3311a504254a7e941fc0578d9726
shichao <315046509@qq.com>
2014-06-02 21:35:15 +0800

    提交

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Weixin::Application.config.secret_key_base = 'bd7a808ed71525891bf26dced3bc7b1208a416ffb7beed79b3ca3338950e422ccd3f89eb16e63e1e27b5e802592e2ed092097baf3eb13b6299ac9130e883ff5d'
