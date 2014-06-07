742c9bf507dcd1fd6b9967a1465813d451218acc
shichao <315046509@qq.com>
2014-06-07 16:58:52 +0800

    提

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ActiveRecord::Base
-  validates :title, :presence => {:message => "请填写标题"}, :length => {:maximum => 15, :message => "标题必须小于15个字"}
+  validates :title, :presence => {:message => "请填写标题"}, :length => {:maximum => 22, :message => "标题必须小于22个字"}
   validates :description, :presence => {:message => "请填写内容"}
   scope :order_ct_desc, lambda { order("created_at DESC") }
 end
