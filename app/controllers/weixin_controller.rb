3fa17a54e08bcc84c606cc0d5d9ace978175bc99
shichao <315046509@qq.com>
2014-06-08 14:27:23 +0800

    提交

--- app/controllers/weixin_controller.rb
+++ app/controllers/weixin_controller.rb
@@ -2,9 +2,12 @@ class WeixinController < ApplicationController
   layout 'weixin'
 
   def index
+    @questions = Question.order_ct_desc.page(params[:page]).per(4)
+    @roll = Case.all.order_ct_desc.page(params[:page]).per(4)
   end
 
   def contrast
-
+    @questions = Question.order_ct_desc.page(params[:page]).per(4)
+    @roll = Case.all.order_ct_desc.page(params[:page]).per(4)
   end
 end
