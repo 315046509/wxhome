eb9209a7e78c6ce500fb2e11ee8868b19677da2e
shichao <315046509@qq.com>
2014-06-11 12:12:37 +0800

    tijiao

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,6 +2,6 @@ class MainController < ApplicationController
   def index
     @questions = Question.order_ct_desc.page(params[:page]).per(3)
     @journalisms = Journalism.order_ct_desc.page(params[:page]).per(10)
-    @roll = Case.all.order_ct_desc.page(params[:page]).per(19)
+    @roll = Case.all.order_ct_desc.page(params[:page]).per(16)
   end
 end
