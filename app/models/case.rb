68abff5d1d216887b4d578ccd444e3dbee0ead7f
shichao <315046509@qq.com>
2014-06-11 16:12:01 +0800

    提交

--- app/models/case.rb
+++ app/models/case.rb
@@ -3,7 +3,7 @@ class Case < ActiveRecord::Base
   validates :content, :presence => {:message => "案例内容不能为空！"}, :length => {:maximum => 200, :message => "案例内容长度不能超过200"}
 
   attr_accessor :avatar
-  has_attached_file :avatar, :styles => {:zhengchang => "350x210#", :thumb => "70x42#", :anli => "219x132#"}, :default_url => "/images/:style/missing.png",
+  has_attached_file :avatar, :styles => {:zhengchang => "350x210>", :thumb => "70x42>", :anli => "480x800>"}, :default_url => "/images/:style/missing.png",
                     :url => "/case/:id_partition/:style/:filename"
   validates_attachment_size :avatar, :less_than => 2.megabytes
   validates_attachment_content_type :avatar, :content_type => ['image/png', 'image/x-png', 'image/jpeg', 'image/pjpeg', 'image/jpg']
