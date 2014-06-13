66a3d8a79c6763933ee0b3dc98407aeb2c020f0a
shichao <315046509@qq.com>
2014-06-13 18:40:06 +0800

    提交

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -1,5 +1,6 @@
 class MainsController < ApplicationController
   before_action :set_main, only: [:show, :edit, :update, :destroy]
+  layout 'admin_no_sider'
 
   # GET /mains
   # GET /mains.json
@@ -26,33 +27,32 @@ class MainsController < ApplicationController
     respond_to do |format|
       if @main.save
         format.html { redirect_to @main, notice: '您已注册成功，请点击返回首页' }
-        format.json { render action: 'show', status: :created, location: @main }
+        format.json { render :show, status: :created, location: @main }
       else
-        format.html { render action: 'new' }
+        format.html { render :new }
         format.json { render json: @main.errors, status: :unprocessable_entity }
       end
     end
   end
-
 
   # DELETE /mains/1
   # DELETE /mains/1.json
   def destroy
     @main.destroy
     respond_to do |format|
-      format.html { redirect_to mains_url }
+      format.html { redirect_to mains_url, notice: 'Main was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_main
-      @main = Main.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_main
+    @main = Main.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def main_params
-      params.require(:main).permit(:name)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def main_params
+    params.require(:main).permit(:name, :tel, :address)
+  end
 end
